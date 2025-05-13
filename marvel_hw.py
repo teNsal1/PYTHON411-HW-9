8f072c77b29908d42ee68c0c02d72be0380739ad
teNsal1 <tensal@bk.ru>
2025-05-13 14:36:07 +0300

    Ввод от пользователя

--- marvel_hw.py
+++ marvel_hw.py
@@ -1,3 +1,11 @@
 from marvel import full_dict
 from pprint import pprint
 
+# Задание 2: Обработка ввода пользователя
+def process_input():
+    user_input = input("Введите id фильмов через пробел(например:3 8 15 23): ")
+    parts = user_input.split()
+    
+    # Преобразуем элементы в int, заменяя нечисловые на None
+    processed = map(lambda x: int(x) if x.isdigit() else None, parts)
+    return list(processed)
